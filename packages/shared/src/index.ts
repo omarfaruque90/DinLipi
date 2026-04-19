d78bb3e002ed748ab8e60842053bbc4652d71818
Md Omar Faruque <omarfaruqe801@gmail.com>
2026-04-19 20:31:28 +0600

    Delete packages/shared directory

--- packages/shared/src/index.ts
+++
@@ -1,5 +0,0 @@
-export type TransactionType = 'expense' | 'income' | 'transfer' | 'deposit' | 'cash_out';
-
-export type CurrencyCode = 'BDT' | 'USD' | 'EUR' | 'GBP' | 'INR';
-
-export type LanguageCode = 'en' | 'bn' | 'banglish';
